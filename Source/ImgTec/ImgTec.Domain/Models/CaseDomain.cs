8215b2e86f156d9c83c319833e9dd2ab8a52d7cc
Adrian Ilewicz <Adrian.Ilewicz@flendr.com>
2015-12-12 12:50:57 +0000

    Gitignore

---
+++ Source/ImgTec/ImgTec.Domain/Models/CaseDomain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImgTec.Domain.Models
+{
+    public class CaseDomain
+    {
+    }
+}
---
+++ Source/ImgTec/ImgTec.Domain/Models/UserDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ImgTec.Domain.Models
+{
+    public class UserDomain : BaseIdentityUser
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public ICollection<CaseDomain> Cases { get; set; }
+    }
+
+    public class BaseIdentityUser : IUser<String>
+    {
+    }
+}
